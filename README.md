8f862c9ce60ee1a48984353a9abb1cff8eea76f4
gracieuxsikuly <graciersikuly@gmail.com>
2026-01-12 16:34:58 +0200

    mis en jours

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # creation de la varibale d'environnement
 py -m venv .env
 # activation de la variable d'environnement
-.\.env\Scripts\Activate.ps1
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+.env\Scripts\activate.ps1
 # installation de dependendace
 pip install -r requirements.txt
--- main.py
+++ main.py
@@ -1,37 +1,46 @@
 import boto3
 import os
-from io import BytesIO
-import geopandas as gpd
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 # configuration de notre compte aws
-AWS_PROFIL= "compteprive"
+AWS_PROFIL= "default"
 AWS_REGION= "us-east-1"
-BUCKET_NAME ="s3-palmiers-projects"
+BUCKET_NAME ="mbau-gracier"
+# BUCKET_NAME ="mbau-gracieux-prive"
 FOLDERS =["raw/","processed/","validated/"]
-# fichier a uploader sur s3
-LOCAL_FILE= "data/AviationDB_V01.gpkg"
-RAW_KEY= "raw/"+os.path.basename(LOCAL_FILE)
+# dossier des me fichier
+LOCAL_DIR = "data"
 # Session aws pour la connexion
 session=boto3.Session(profile_name=AWS_PROFIL,region_name=AWS_REGION)
 s3=session.client('s3')
-# creation de notre bucket et nous allons activer le versionnage
-try:
-    s3.create_bucket(Bucket=BUCKET_NAME)
-    print(f"bucket{BUCKET_NAME} est creer avec success")
-except s3.exceptions.BucketAlreadyOwnedByYou:
-    print(f"bucket {BUCKET_NAME} est deja cree")
-    s3.put_bucket_versioning(
-        Bucket=BUCKET_NAME,
-        VersioningConfiguration={'status':'enabled'}
-    )
-print('versionning active')
-# on creer la structure des mes dossiers
-for folder in FOLDERS:
-    s3.put_object(Bucket=BUCKET_NAME,key=folder)
-print(f"les dossiers creer: {FOLDERS}")
-# upload des fichiers dans le dossier
-if os.path.isfile(LOCAL_FILE):
-    s3.upload_file(LOCAL_FILE,BUCKET_NAME,RAW_KEY)
-    print(f'fichier uploader: s3//{BUCKET_NAME}/{RAW_KEY}')
-else:
-    print(f'fichier introuvable {LOCAL_FILE}')
+# DECLARATIONS DES MES FONCTIONS
+def creation_bucket():
+    # creation de notre bucket et nous allons activer le versionnage
+    try:
+        s3.create_bucket(Bucket=BUCKET_NAME)
+        logging.info("bucket"+ BUCKET_NAME+ " est creer avec success")
+        s3.put_bucket_versioning(
+            Bucket=BUCKET_NAME,
+            VersioningConfiguration={"Status": "Enabled"}
+        )
+        logging.info("versionning active")
+    except s3.exceptions.BucketAlreadyOwnedByYou:
+        logging.warning("bucket"+ BUCKET_NAME + "est deja cree")
+
+def upload_fichier():
+    # création des dossiers S3
+    for folder in FOLDERS:
+        s3.put_object(Bucket=BUCKET_NAME, Key=folder)
+    logging.info("dossiers créés ")
+    # upload de mes fichiers sur mon bucket
+    for filename in os.listdir(LOCAL_DIR):
+        local_file = os.path.join(LOCAL_DIR, filename)
+        if os.path.isfile(local_file):
+            s3_key = f"raw/{filename}"
+            s3.upload_file(local_file, BUCKET_NAME, s3_key)
+            logging.info("fichier uploadé : s3://" + BUCKET_NAME+ "/"+s3_key)
+# APPEL DES MES FONCTIONS
+creation_bucket()
+upload_fichier()
